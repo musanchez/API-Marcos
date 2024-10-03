7bea21c467427e9c22acc19d28ad60d4b526fb2b
musanchez <musanchez@uamv.edu.ni>
2024-10-03 16:09:29 -0600

    escribo

--- api-marcos/resources/views/pdf/document.blade.php
+++ api-marcos/resources/views/pdf/document.blade.php
@@ -4,13 +4,58 @@
 <head>
     <meta charset="UTF-8">
     <title>{{ $title }}</title>
-    <!-- Incluyendo Bootstrap -->
-    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    <style>
+        /* Incluyendo Bootstrap en línea para que DomPDF lo procese */
+        /* Esto es una parte esencial de Bootstrap CSS */
+        .container {
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+            margin-right: auto;
+            margin-left: auto;
+        }
+
+        .table {
+            width: 100%;
+            margin-bottom: 1rem;
+            background-color: transparent;
+        }
+
+        .table-bordered {
+            border: 1px solid #dee2e6;
+        }
+
+        .table-striped tbody tr:nth-of-type(odd) {
+            background-color: rgba(0, 0, 0, 0.05);
+        }
+
+        .thead-dark th {
+            color: #fff;
+            background-color: #343a40;
+            border-color: #454d55;
+        }
+
+        .table th, .table td {
+            padding: 0.75rem;
+            vertical-align: top;
+            border-top: 1px solid #dee2e6;
+        }
+
+        h1 {
+            text-align: center;
+            margin: 40px 0;
+            font-size: 2.5rem;
+        }
+
+        body {
+            font-family: 'DejaVu Sans', sans-serif;
+        }
+    </style>
 </head>
 
 <body>
     <div class="container">
-        <h1 class="text-center my-4">{{ $title }}</h1>
+        <h1>{{ $title }}</h1>
         <table class="table table-bordered table-striped">
             <thead class="thead-dark">
                 <tr>
